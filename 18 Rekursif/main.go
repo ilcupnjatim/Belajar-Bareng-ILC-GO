1bc5e1afca95f0af5063a883e8592662d840838a
Rakhid16 <roploverz@gmail.com>
2020-11-01 14:04:33 +0700

    Pekan ketiga

--- 18 Rekursif/main.go
+++ 18 Rekursif/main.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 // FUNGSI YANG MEMANGGIL DIRINYA SENDIRI
-
-func Faktorial(n int) int {
-	if n == 0 {
+func Faktorial(angka int) int {
+	if angka == 0 {
 		return 1
 	}
 
-	return n * Faktorial(n-1)
+	return angka * Faktorial(angka-1)
+}
+
+// CONTOH DUA
+func Fibonanci(angka int) int {
+	if angka == 0 {
+		return 0
+	} else if angka == 1 {
+		return 1
+	} else {
+		return Fibonanci(angka-1) + Fibonanci(angka-2)
+	}
 }
 
 func main() {
-	fmt.Println(Faktorial(7))
+	fmt.Println(Faktorial(5))
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(Fibonanci(i))
+	}
 }
