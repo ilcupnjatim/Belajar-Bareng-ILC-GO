b5278b17b15380dacb59d9f204b6a2a24a7eefd1
Rakhid16 <roploverz@gmail.com>
2020-11-01 14:01:51 +0700

    Pekan ketiga

--- 16 Variadic Functions/main.go
+++ 16 Variadic Functions/main.go
@@ -6,29 +6,34 @@ import (
 )
 
 // SIMPLE VARIADIC FUNCTION
-func Tambah(nums ...int) {
-	fmt.Println(nums)
-	fmt.Println(reflect.TypeOf(nums))
+func Himpunan_angka(angka ...int) {
+	fmt.Println(angka)
+	fmt.Println(reflect.TypeOf(angka))
 }
 
 // VARIADIC FUNCTION DENGAN PARAMETER LEBIH DARI 1
-func Pembagi(pembagi int, acak ...int) float32 {
+func Pembagi(pembagi int, angka_acak ...int) float32 {
 	total := 0
 
-	for _, num := range acak {
+	for _, num := range angka_acak {
 		total += num
 	}
 
 	return float32(total / pembagi)
 }
 
+// func Nama_dan_angka(nama ...string, angka_acak ...int) {
+// 	fmt.Println(nama)
+// 	fmt.Println(angka)
+// }
+
 func main() {
-	Tambah(1, 2, 3)
+	Himpunan_angka(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-	angka := []int{1, 2, 3, 4}
-	Tambah(angka...)
+	hasil := Pembagi(5, 1, 2, 3, 4, 5)
+	fmt.Println(hasil)
 
-	fmt.Println(Pembagi(5, 6, 7, 8, 9))
+	//Nama_dan_angka("Jojo", "Naruto", "Ichigo", 12, 21, 666)
 }
 
 // VARIADIC FUNCTION DENGAN PARAMETER BEDA TIPE DATA DAN ELEMEN TAK TERBATAS?
